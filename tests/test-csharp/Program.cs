8e7ce7e57416b5d49eead1861aa4877b23658163
Eric L. Hernes <eric@qinc.tv>
2023-08-19 06:57:31 -0600

    Push changes for David's initial Commander release

--- tests/test-csharp/Program.cs
+++ tests/test-csharp/Program.cs
@@ -1,59 +1,98 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Threading;
 
-static class NativeMethods
+class Whb04bNative
 {
-    [DllImport("kernel32.dll")]
+    private delegate int EventCallback(byte b1, byte b2, byte axis, byte stepcon, int count);
+    private EventCallback _eventDelegate;
+    private IntPtr _native;
+
+    private const string _whb04b_dll = @"\\vmware-host\Shared Folders\Documents\Visual Studio 2015\Projects\whb04b\x64\Debug\whb04b.dll";
+    //    private const string _whb04b_dll = @"whb04b.dll";
+    /*
+    [DllImport(_whb04b_dll, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr whb04b_create(EventCallback fn);
+    [DllImport(_whb04b_dll, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    private static extern void whb04b_destroy(IntPtr vp);
+    [DllImport(_whb04b_dll, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    private static extern void whb04b_display(IntPtr vp, bool wm_coord, double axis1, double axis2, double axis3, double feed, double spindle);
+    */
+    
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)] private delegate IntPtr whb04b_create_c(EventCallback fn);
+    whb04b_create_c whb04b_create;
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)] private delegate void whb04b_destroy_c(IntPtr instance);
+    whb04b_destroy_c whb04b_destroy;
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)] private delegate bool whb04b_display_c(IntPtr vp, bool wm_coord, double axis1, double axis2, double axis3, double feed, double spindle);
+    whb04b_display_c whb04b_display;
+
+
+    [DllImport("kernel32.dll", SetLastError = true)]
     public static extern IntPtr LoadLibrary(string dllToLoad);
+    [DllImport("kernel32.dll")]
+    public static extern int GetLastError();
     [DllImport("kernel32.dll")]
     public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
     [DllImport("kernel32.dll")]
     public static extern bool FreeLibrary(IntPtr hModule);
+
+    private int PendantEvent(byte b1, byte b2, byte axis, byte stepcon, int count)
+    {
+        Console.WriteLine("Pendant Event: (b1 {0}) (b2 {1}) (axis {2}) (stepcon {3}) (count {4})", b1, b2, axis, stepcon, count);
+        return 1;
+    }
+
+    public Whb04bNative()
+    {
+        IntPtr pDll = LoadLibrary(_whb04b_dll);
+        if (pDll == IntPtr.Zero)
+        {
+            Console.WriteLine("LoadLibrary({0}): {1}", _whb04b_dll, Marshal.GetLastWin32Error().ToString());
+        }
+        else
+        {
+            IntPtr createAddr = GetProcAddress(pDll, "whb04b_create");
+            whb04b_create = (whb04b_create_c)Marshal.GetDelegateForFunctionPointer(createAddr, typeof(whb04b_create_c));
+
+            IntPtr destroyAddr = GetProcAddress(pDll, "whb04b_destroy");
+            whb04b_destroy = (whb04b_destroy_c)Marshal.GetDelegateForFunctionPointer(destroyAddr, typeof(whb04b_destroy_c));
+
+            IntPtr displayAddr = GetProcAddress(pDll, "whb04b_display");
+            whb04b_display = (whb04b_display_c)Marshal.GetDelegateForFunctionPointer(displayAddr, typeof(whb04b_display_c));
+
+            _eventDelegate = new EventCallback(PendantEvent);
+            _native = whb04b_create(_eventDelegate);
+        }
+    }
+
+    ~Whb04bNative()
+    {
+        if (_native != IntPtr.Zero)
+        {
+           whb04b_destroy(_native);
+        }
+    }
+    public void display(bool wm_coord, double axis1, double axis2, double axis3, double feed, double spindle)
+    {
+ //       whb04b_display(_native, wm_coord, axis1, axis2, axis3, feed, spindle);
+    }
 }
 
 namespace test_csharp
 {
     class Program
     {
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate IntPtr whb04b_create_c();
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate void whb04b_destroy_c(IntPtr instance);
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-	private delegate bool whb04b_display_c(IntPtr vp, bool wm_coord, double axis1, double axis2, double axis3, double feed, double spindle);
-
-        static void Main(string[] args)
+            static void Main(string[] args)
         {
+            Whb04bNative whb04b = new Whb04bNative();
+                //	        whb04b_display(instance, true, 12.34, 23.45, 34.56, 3785, 19783);
 
-	IntPtr pDll = NativeMethods.LoadLibrary(@"\\vmware-host\Shared Folders\Documents\Visual Studio 2015\Projects\whb04b\whb04b\Debug\whb04b.dll");
-//            IntPtr pDll = NativeMethods.LoadLibrary(@"whb04b.dll");
-
-	    if (pDll == IntPtr.Zero) {
-	    } else {
-            IntPtr createAddr = NativeMethods.GetProcAddress(pDll, "whb04b_create");
-	    whb04b_create_c whb04b_create = (whb04b_create_c)Marshal.GetDelegateForFunctionPointer(createAddr, typeof(whb04b_create_c));
-	    
-            IntPtr destroyAddr = NativeMethods.GetProcAddress(pDll, "whb04b_destroy");
-	    whb04b_destroy_c whb04b_destroy = (whb04b_destroy_c)Marshal.GetDelegateForFunctionPointer(destroyAddr, typeof(whb04b_destroy_c));
-	    
-            IntPtr displayAddr = NativeMethods.GetProcAddress(pDll, "whb04b_display");
-	    whb04b_display_c whb04b_display = (whb04b_display_c)Marshal.GetDelegateForFunctionPointer(displayAddr, typeof(whb04b_display_c));
-
-	    IntPtr instance = whb04b_create();
-	    whb04b_display(instance, true, 12.34, 23.45, 34.56, 3785, 19783);
-
-	    whb04b_destroy(instance);
-
-            //if(pAddressOfFunctionToCall == IntPtr.Zero)
-
-            bool result = NativeMethods.FreeLibrary(pDll);
-            //remaining code here
-//            Console.WriteLine(theResult);
+                for (;;)
+                {
+                Thread.Sleep(2000);
+                }
+            
         }
     }
-}
 }
--- whb04b/whb04b-win32.cpp
+++ whb04b/whb04b-win32.cpp
@@ -14,12 +14,20 @@
  *
  */
 
+//#define STUB_ONLY
+
 #include <windows.h>
 #include "../whb04b.h"
 #include "../whb04b-win32.h"
-#include <future>
 
+#ifndef STUB_ONLY
+#include <future>
 static std::future<void> s_runFuture;
+#endif
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+  return true;
+}
 
 class WinDelegate : public WHB04B::Delegate {
 public:
@@ -35,23 +43,31 @@ static WinDelegate *s_wdp = nullptr;
 void*
 whb04b_create(PendantEventCallback cb) {
   s_wdp = new WinDelegate(cb);
+#ifndef STUB_ONLY
   WHB04B *whb = new WHB04B(s_wdp);
   s_runFuture = std::async(std::launch::async, &WHB04B::run, whb);
   return (void*) whb;
+#else
+  return 0;
+#endif
 }
 
 void
 whb04b_destroy(void *vp) {
   WHB04B *whb = (WHB04B*)vp;
+#ifndef STUB_ONLY
   whb->stop();
   s_runFuture.wait();
   delete whb;
   delete s_wdp;
+#endif
 }
 
 bool
 whb04b_display(void *vp, bool wm_coord, double axis1, double axis2, double axis3, double feed, double spindle) {
+#ifndef STUB_ONLY
   ((WHB04B*)vp)->display(wm_coord, axis1, axis2, axis3, feed, spindle);
+#endif
   return TRUE;
 }
 
